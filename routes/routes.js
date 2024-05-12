6ea2f1f6f454344697e7793d7185e2da6ab11da9
Dhiyanesh2003 <dhiyanesh7338942092@gmail.com>
2024-05-12 05:58:39 +0530

    sf'DS

--- routes/routes.js
+++ routes/routes.js
@@ -6,6 +6,9 @@ const { googleAuth, verifyAuth } = require('../controllers/authController')
 const verifyToken = require('../middlewares/auth')
 
 router.post("/auth", googleAuth);
+router.get("/auth", (req, res) => {
+    res.send("Hello");
+});  
 router.post("/verifyAuth", verifyToken , verifyAuth);
 
 module.exports = router;
